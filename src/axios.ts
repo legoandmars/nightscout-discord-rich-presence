6551964edc3d1325c45a35cba67f72a2fd19f040
Jack Baron <jackmbaron@gmail.com>
2019-09-26 08:13:32 +0100

    fix package.json resolution in builds

--- src/axios.ts
+++ src/axios.ts
@@ -1,10 +1,16 @@
 import Axios from 'axios'
-import readPkg from 'read-pkg'
+import { sync as readPkgSync } from 'read-pkg-up'
+import log from './log'
 
-const pkg = readPkg.sync()
+const pkg = readPkgSync({ cwd: __dirname })
+if (pkg === undefined) {
+  log.error('Could not find `package.json`', 1)
+  throw new Error('missing package')
+}
+
 const axios = Axios.create({
   headers: {
-    'User-Agent': `${pkg.name}/${pkg.version}`,
+    'User-Agent': `${pkg.package.name}/${pkg.package.version}`,
   },
 })
 
